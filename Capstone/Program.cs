6d51628e1d18868cb8763c2b6413e0a5082b5e3e
James Marchant <marcha5@mail.nmc.edu>
2020-12-05 21:57:41 -0500

    /initial push

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -49,12 +49,12 @@ namespace Capstone
             Console.CursorVisible = true;
             List<string> wordToGuess = new List<string>();
             List<string> hiddenWord = new List<string>();
-            int numberOfGuesses = 0;
+            List<string> lettersGuessed = new List<string>();
+            int numberOfGuesses = 6;
 
             bool quitApplication = false;
+            bool gameEnd = false;
             string menuChoice;
-
-
 
             do
             {
@@ -65,6 +65,7 @@ namespace Capstone
                 //
                 Console.WriteLine("\ta) Set Game Parameters");
                 Console.WriteLine("\tb) Play the game");
+                Console.WriteLine("\tc) Reset Parameters");
                 Console.WriteLine("\tq) Quit");
                 Console.Write("\t\tEnter Choice:");
                 menuChoice = Console.ReadLine().ToLower();
@@ -75,14 +76,46 @@ namespace Capstone
                 switch (menuChoice)
                 {
                     case "a":
-                        wordToGuess = DisplayChangeWordToGuessScreen(wordToGuess);
-                        numberOfGuesses = DisplayNumberOfGuessesScreen(numberOfGuesses);
-                        hiddenWord = DisplayInitializeHiddenWord(wordToGuess, hiddenWord);
+                        if (gameEnd == true)
+                        {
+                            Console.WriteLine("\t Please reset the game parameters.");
+                            DisplayContinuePrompt();
+                        }
+                        else
+                        {
+                            wordToGuess = DisplayChangeWordToGuessScreen(wordToGuess);
+                            numberOfGuesses = DisplayNumberOfGuessesScreen(numberOfGuesses);
+                            hiddenWord = DisplayInitializeHiddenWord(wordToGuess, hiddenWord);
+                        }
 
                         break;
 
                     case "b":
-                        DisplayPlayGameScreen(wordToGuess, hiddenWord, numberOfGuesses);
+                        if (numberOfGuesses == 6)
+                        {
+                            Console.WriteLine("\tPlease set game parameters.");
+                            DisplayContinuePrompt();
+                        }
+                        else if (gameEnd == true)
+                        {
+                            Console.WriteLine("\t Please reset the game parameters.");
+                            DisplayContinuePrompt();
+                        }
+                        else
+                        {
+                            gameEnd = DisplayPlayGameScreen(wordToGuess, hiddenWord, numberOfGuesses, lettersGuessed, gameEnd);
+                        }
+
+                        break;
+
+                    case "c":
+                        wordToGuess = DisplayResetWordToGuess(wordToGuess);
+                        hiddenWord = DisplayResetHiddenWord(hiddenWord);
+                        numberOfGuesses = DisplayResetNumberOfGuesses(numberOfGuesses);
+                        lettersGuessed = DisplayResetLettersGuessed(lettersGuessed);
+                        gameEnd = DisplayResetGameEnd(gameEnd);
+                        Console.WriteLine("\tGame parameters have been reset.");
+                        DisplayContinuePrompt();
                         break;
 
                     case "q":
@@ -100,8 +133,54 @@ namespace Capstone
             } while (!quitApplication);
         }
 
+        #region RESET
+
+        //
+        // these methods all set one variable back to its starting value
+        //
+
+        static bool DisplayResetGameEnd(bool gameEnd)
+        {
+            gameEnd = false;
+
+            return gameEnd;
+        }
+
+        static int DisplayResetNumberOfGuesses(int numberOfGuesses)
+        {
+            numberOfGuesses = 6;
+
+            return numberOfGuesses;
+        }
+
+        static List<string> DisplayResetLettersGuessed(List<string> lettersGuessed)
+        {
+            lettersGuessed.Clear();
+
+            return lettersGuessed;
+        }
+
+        static List<string> DisplayResetHiddenWord(List<string> hiddenWord)
+        {
+            hiddenWord.Clear();
+
+            return hiddenWord;
+        }
+
+        static List<string> DisplayResetWordToGuess(List<string> wordToGuess)
+        {
+            wordToGuess.Clear();
+
+            return wordToGuess;
+        }
+        #endregion
+
+        #region HANGMAN
         static List<string> DisplayInitializeHiddenWord(List<string> WordToGuess, List<string> HiddenWord)
         {
+            //
+            // this method intializes the list that will be displayed to the user and tested to se if the wor dis done being guessed
+            //
             string underscoreToAdd;
             foreach (string Letter in WordToGuess)
             {
@@ -136,6 +215,9 @@ namespace Capstone
                     Console.WriteLine("\tPlease enter a number between 1 and 5.");
                 }
             } while (!validResponse);
+            //
+            // display number of guesses to user
+            //
             numberOfGuesses = possibleNumber;
             Console.WriteLine($"\tNumber of guesses:{numberOfGuesses}");
             DisplayContinuePrompt();
@@ -146,6 +228,7 @@ namespace Capstone
         {
             bool isDoneAdding = false;
             bool validResponse = false;
+            int checkNumber;
             string userResponse;
             HangmanAlphabet letter;
 
@@ -178,10 +261,18 @@ namespace Capstone
                 {
                     isDoneAdding = true;
                 }
+                else if (int.TryParse(userResponse, out checkNumber))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("\tPlease enter a valid letter");
+                    Console.WriteLine("\tPress any key to continue.");
+                    Console.ReadKey();
+                    validResponse = false;
+                }
                 else if (!Enum.TryParse(userResponse, out letter))
                 {
                     Console.WriteLine();
-                    Console.WriteLine("\tPlease enter a valid command");
+                    Console.WriteLine("\tPlease enter a valid letter");
                     Console.WriteLine("\tPress any key to continue.");
                     Console.ReadKey();
                     validResponse = false;
@@ -191,6 +282,10 @@ namespace Capstone
                     WordToGuess.Add(letter.ToString());
                 }
             } while (!validResponse || !isDoneAdding);
+            //
+            // display finished word to user
+            //
+            Console.WriteLine();
             Console.Write("\tCurrent Word:");
             foreach (string Letter in WordToGuess)
             {
@@ -200,32 +295,183 @@ namespace Capstone
             return WordToGuess;
         }
 
-        static void DisplayPlayGameScreen(List<string> wordToGuess, List<string> hiddenWord, int numberOfGuesses)
+        static bool DisplayPlayGameScreen(List<string> wordToGuess, List<string> hiddenWord, int numberOfGuesses, List<string> lettersGuessed, bool gameEnd)
         {
-            bool gameEnd = false;
             do
             {
+                bool isWordGuessed = false;
                 DisplayScreenHeader("\tHangman");
                 //
                 //display all parameters to player
                 //
+
                 Console.WriteLine($"\tNumber of incorect guesses left: {numberOfGuesses}");
-                Console.WriteLine("\tCurrent word:");
+                Console.Write("\tCurrent word:");
                 Console.Write("\t");
                 foreach (string letter in hiddenWord)
                 {
                     Console.Write(letter + " ");
                 }
-                numberOfGuesses = DisplayGuessMechanic(wordToGuess, hiddenWord, numberOfGuesses);
+                Console.WriteLine();
+                switch (numberOfGuesses.ToString())
+                {
+                    case "0":
+                        Console.WriteLine("\t----");
+                        Console.WriteLine("\t|  |");
+                        Console.WriteLine("\t|  O");
+                        Console.WriteLine("\t| -|-");
+                        Console.WriteLine(@"\t|/ \");
+                        break;
+
+                    case "1":
+                        Console.WriteLine("\t----");
+                        Console.WriteLine("\t|  |");
+                        Console.WriteLine("\t|  O");
+                        Console.WriteLine("\t| -|-");
+                        Console.WriteLine("\t|  ");
+                        break;
+                    case "2":
+                        Console.WriteLine("\t----");
+                        Console.WriteLine("\t|  |");
+                        Console.WriteLine("\t|  O");
+                        Console.WriteLine("\t| -|");
+                        Console.WriteLine("\t|  ");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("\t----");
+                        Console.WriteLine("\t|  |");
+                        Console.WriteLine("\t|  O");
+                        Console.WriteLine("\t|  |");
+                        Console.WriteLine("\t|");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("\t----");
+                        Console.WriteLine("\t|  |");
+                        Console.WriteLine("\t|  O");
+                        Console.WriteLine("\t|");
+                        Console.WriteLine("\t|");
+                        break;
+
+                    case "5":
+                        Console.WriteLine("\t----");
+                        Console.WriteLine("\t|  |");
+                        Console.WriteLine("\t| ");
+                        Console.WriteLine("\t|");
+                        Console.WriteLine("\t|");
+                        break;
+                }
+                Console.WriteLine("\tLetters that have been guessed.");
+                Console.Write("\t");
+                for (int index = 0; index < lettersGuessed.Count; index++)
+                {
+                    Console.Write(lettersGuessed[index]);
+                    Console.Write(" ");
+                }
+                numberOfGuesses = DisplayGuessMechanic(wordToGuess, hiddenWord, lettersGuessed, numberOfGuesses);
+                if (numberOfGuesses == 0)
+                {
+                    gameEnd = true;
+                }
+                isWordGuessed = DisplayCheckWordGuessed(hiddenWord, isWordGuessed);
+                if (isWordGuessed == true)
+                {
+                    gameEnd = true;
+                }
             } while (!gameEnd);
+            Console.Clear();
+            DisplayScreenHeader("\tHangman");
+            Console.WriteLine($"\tNumber of incorect guesses left: {numberOfGuesses}");
+            Console.Write("\tCurrent word:");
+            Console.Write("\t");
+            foreach (string letter in hiddenWord)
+            {
+                Console.Write(letter + " ");
+            }
+            Console.WriteLine();
+            switch (numberOfGuesses.ToString())
+            {
+                case "0":
+                    Console.WriteLine("\t----");
+                    Console.WriteLine("\t|  |");
+                    Console.WriteLine("\t|  O");
+                    Console.WriteLine("\t| -|-");
+                    Console.WriteLine("\t| // ");
+                    break;
 
+                case "1":
+                    Console.WriteLine("\t----");
+                    Console.WriteLine("\t|  |");
+                    Console.WriteLine("\t|  O");
+                    Console.WriteLine("\t| -|-");
+                    Console.WriteLine("\t|  ");
+                    break;
+                case "2":
+                    Console.WriteLine("\t----");
+                    Console.WriteLine("\t|  |");
+                    Console.WriteLine("\t|  O");
+                    Console.WriteLine("\t| -|");
+                    Console.WriteLine("\t|  ");
+                    break;
 
+                case "3":
+                    Console.WriteLine("\t----");
+                    Console.WriteLine("\t|  |");
+                    Console.WriteLine("\t|  O");
+                    Console.WriteLine("\t|  |");
+                    Console.WriteLine("\t|");
+                    break;
+
+                case "4":
+                    Console.WriteLine("\t----");
+                    Console.WriteLine("\t|  |");
+                    Console.WriteLine("\t|  O");
+                    Console.WriteLine("\t|");
+                    Console.WriteLine("\t|");
+                    break;
+
+                case "5":
+                    Console.WriteLine("\t----");
+                    Console.WriteLine("\t|  |");
+                    Console.WriteLine("\t| ");
+                    Console.WriteLine("\t|");
+                    Console.WriteLine("\t|");
+                    break;
+            }
+            Console.WriteLine();
+            if (numberOfGuesses == 0)
+            {
+                Console.WriteLine("\tYou have lost.");
+            }
+            else
+            {
+                Console.WriteLine("\tYou have won.");
+            }
+            Console.WriteLine("\tPlease reset the game to play again, otherwise please exit the application.");
             DisplayContinuePrompt();
 
+            return gameEnd;
 
         }
 
-        static int DisplayGuessMechanic(List<string> wordToGuess, List<string> hiddenWord, int numberOfGuesses)
+        static bool DisplayCheckWordGuessed(List<string> hiddenWord, bool isWordGuessed)
+        {
+            //
+            // check to see if any underscores are left in the list
+            //
+            isWordGuessed = true;
+            for (int index = 0; index < hiddenWord.Count; index++)
+            {
+                if (hiddenWord[index] == "_")
+                {
+                    isWordGuessed = false;
+                }
+            }
+            return isWordGuessed;
+        }
+
+        static int DisplayGuessMechanic(List<string> wordToGuess, List<string> hiddenWord, List<string> lettersGuessed, int numberOfGuesses)
         {
             string letterBeingGuessed;
             bool validResponse = false;
@@ -233,7 +479,7 @@ namespace Capstone
             Console.WriteLine();
             Console.WriteLine();
             Console.Write("\tEnter a letter to guess:");
-            
+
 
             //
             // have player enter a letter, test to see if it is a letter
@@ -242,10 +488,12 @@ namespace Capstone
             do
             {
                 letterBeingGuessed = Console.ReadLine().ToUpper();
+
                 if (Enum.TryParse(letterBeingGuessed, out letter))
                 {
                     hiddenWord = DisplayChangeHiddenWord(wordToGuess, hiddenWord, letterBeingGuessed);
-                    numberOfGuesses = DisplayCorrectGuessCheck(hiddenWord, letterBeingGuessed, numberOfGuesses);
+                    numberOfGuesses = DisplayCorrectGuessCheck(hiddenWord, letterBeingGuessed, lettersGuessed, numberOfGuesses);
+                    lettersGuessed.Add(letterBeingGuessed);
                     validResponse = true;
                 }
                 else
@@ -261,24 +509,47 @@ namespace Capstone
             return numberOfGuesses;
         }
 
-        static int DisplayCorrectGuessCheck(List<string> hiddenWord, string letterBeingGuessed, int numberOfGuesses)
+        static int DisplayCorrectGuessCheck(List<string> hiddenWord, string letterBeingGuessed, List<string> lettersGuessed, int numberOfGuesses)
         {
             bool isCorrectGuess = false;
+            bool alreadyGuessedLetter = false;
+            //
+            // test if letter was already guessed
+            //
+            for (int index = 0; index < lettersGuessed.Count; index++)
+            {
+                if (lettersGuessed[index] == letterBeingGuessed)
+                {
+                    alreadyGuessedLetter = true;
+                }
+            }
+            //
+            // if letter was already guessed do no tests
+            //
+            if (alreadyGuessedLetter == true)
+            {
+                Console.WriteLine("\tYou have already guessed this letter.");
+                Console.WriteLine("\tPress any key to continue.");
+                Console.ReadKey();
+            }
             //
             // if letter is in word, do not decrease number of guesses
             //
-
-            for (int index = 0; index < hiddenWord.Count; index++)
+            else if (alreadyGuessedLetter == false)
             {
-                if (letterBeingGuessed == hiddenWord[index])
+                for (int index = 0; index < hiddenWord.Count; index++)
                 {
-                    isCorrectGuess = true;
+                    if (letterBeingGuessed == hiddenWord[index])
+                    {
+                        isCorrectGuess = true;
+                    }
+                }
+                if (isCorrectGuess == false)
+                {
+                    numberOfGuesses = numberOfGuesses - 1;
                 }
             }
-            if (isCorrectGuess == false)
-            {
-                numberOfGuesses = numberOfGuesses - 1;
-            }
+           
             return numberOfGuesses;
         }
 
@@ -296,6 +567,8 @@ namespace Capstone
             }
             return hiddenWord;
         }
+        #endregion
+
         #region USER INTERFACE
 
         // <summary>
@@ -309,12 +582,13 @@ namespace Capstone
 
             Console.Clear();
             Console.WriteLine();
-            Console.WriteLine("\t\tCapstone Project");
+            Console.WriteLine("\t\tHangman");
             Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("\tYou will be playing a game of hangman.");
 
             DisplayContinuePrompt();
         }
-
         // <summary>
         //*****************************************************************
         // *                     Closing Screen                            *
@@ -326,7 +600,7 @@ namespace Capstone
 
             Console.Clear();
             Console.WriteLine();
-            Console.WriteLine("\t\tThank you for using Finch Control!");
+            Console.WriteLine("\t\tThank you for playing Hangman!");
             Console.WriteLine();
 
             DisplayContinuePrompt();
